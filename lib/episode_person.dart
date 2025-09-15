88502142530657f21acbf2ac2679eda0c0ddd5a3
YamatoRyuma <gamerliu2004@gmail.com>
2025-09-15 12:27:01 +0700

    About You-chan part 2

--- lib/episode_person.dart
+++ lib/episode_person.dart
@@ -10,6 +10,50 @@ class AboutEpisodePerson extends StatefulWidget {
 class _AboutEpisodePerson extends State<AboutEpisodePerson> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: [
+              Image.asset('image/photo/you-chan_look.png'),
+
+              Positioned(
+                top: 30,
+                left: 20,
+                child: Card(
+                  color: Colors.black45,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.3),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ]
+          ),
+
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.all(15.6),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [],
+              ),
+            )
+          )
+        ]
+      )
+    );
   }
 }
