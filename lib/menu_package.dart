072ebc0a7ff5c06cbc5c6e7f53bba6129e6acd9f
YamatoRyuma <gamerliu2004@gmail.com>
2025-09-18 06:47:28 +0700

    About You-chan part 3

--- lib/menu_package.dart
+++ lib/menu_package.dart
@@ -131,7 +131,210 @@ class PersonPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 25.5,
+              left: 20.1,
+              right: 20.1
+            ),
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.5),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 10,
+                  offset: Offset(10, 18.5)
+                )
+              ]
+            ),
+
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.5),
+              child: Image.asset('image/photo/you-chan_look.png'),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(
+              top: 50.0,
+              left: 10.5,
+              right: 10.5,
+              bottom: 20.5
+            ),
+
+            child: Table(
+              border: TableBorder.all(color: Color.fromARGB(89, 81, 171, 241)),
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: [
+                TableRow(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 81, 171, 241)
+                  ),
+
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.2),
+                        child: Text(
+                          "Description Name",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.2),
+                        child: Text(
+                          "You-chan Description",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    )
+                  ]
+                ),
+
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Subunit",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Tokumin'
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Image.asset(
+                          'image/CYaRon.png',
+                          height: 50,
+                        ),
+                      ),
+                    )
+                  ]
+                ),
+
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "School",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Tokumin'
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("Uranohoshi Girls' High School"),
+                      ),
+                    ),
+                  ]
+                ),
+
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "School Years",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Tokumin'
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("2nd Years"),
+                      ),
+                    ),
+                  ]
+                ),
+
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Birthday",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Tokumin'
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("April 17"),
+                      ),
+                    ),
+                  ]
+                ),
+
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Astrological sign",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Tokumin'
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("Arise"),
+                      ),
+                    )
+                  ]
+                )
+              ]
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
